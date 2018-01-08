32db3c40e8882ea9bdaff6e94a851a823e713359
daidaidongdong <914510812@qq.com>
2018-01-08 19:27:39 +0800

    hhh

---
+++ public/js/search.js
@@ -0,0 +1,12 @@
+function change(){
+    if($("#find").val()=="学生学号" || $("#find").val()=="学生姓名"){
+        $(".searchBox").show();
+        $(".searchInput").attr('placeholder', $("#find").val());
+        $(".selectBox").hide();
+    }
+    else{
+        $(".searchBox").hide();
+        $(".selectBox").show();
+    }
+}
+
