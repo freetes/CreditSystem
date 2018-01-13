c8e7e9d1f7d6b11cad27bb05e3e60d37f44a026e
lishuang <www.1030415864@qq.com>
2018-01-13 16:57:29 +0800

    1111

--- public/js/excel.js
+++ public/js/excel.js
@@ -4,25 +4,38 @@ $(".updateExcel").click(function(obj){
 });
 
 const handleFile = file=>{
+  //文件格式不符合
   if(file.name.split('.').pop()!=='xlsx'&&file.name.split('.').pop()!=='xls'){
     console.log('文件格式不符合！');
     $("#excelInput").val('');
-
     return;
   }
-  else{
-    console.log('文件格式正确！');
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(file);
 
-    reader.onload = function(e){
-      const workBook = XLSX.read(e.target.result, {
-        type: 'array'
-      });
+  console.log('文件格式正确！');
+  const reader = new FileReader();
+  reader.readAsArrayBuffer(file);
+  reader.onload = function(e){
+    const workBook = XLSX.read(e.target.result, {type: 'array'});
+    const data = workBook.Sheets[workBook.SheetNames[0]];
 
-      const data = workBook.Sheets[workBook.SheetNames[0]];
-      console.log(data);
+    let items = [];
+    let item = [];
+
+    for(let i=2; i<=data["!ref"].split(':')[1].slice(1); i++){
+      item = [];
+      for(let j='A'; j<=data["!ref"].split(':')[1].slice(0, 1); j = String.fromCharCode((j.charCodeAt())+1)){
+        item.push(data[''+j+i].v);
+      }
+      items.push(item);
     }
+    console.log(items);
 
+    
   }
+};
+
+const saveData = data=>{
+  $.ajax({
+    
+  })
 };
