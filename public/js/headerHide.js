56028a7ae558de6d921f99bce00d20343ec21d51
lishuang <www.1030415864@qq.com>
2018-01-14 09:53:50 +0800

    1
    1

--- public/js/headerHide.js
+++ public/js/headerHide.js
@@ -1,13 +1,13 @@
 window.onload = ()=>{
-  $(".title").css("line-height", $(".mainLogo").css("height"));
   if(window.innerWidth<600){
     $(".daidaidongdong").css("display", "none");
+    $(".mlogo").css("display", "none");
   }
 }
 
 window.onresize = ()=>{
-  $(".title").css("line-height", $(".mainLogo").css("height"));
   if(window.innerWidth<600){
     $(".daidaidongdong").css("display", "none");
+    $(".mlogo").css("display", "none");
   }
 }
